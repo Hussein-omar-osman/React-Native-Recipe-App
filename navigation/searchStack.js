bfd30bc191cd73c2e5c0612c9fdd1a4b3b6db2c1
Hussein Omar <husseinomar6190@gmail.com>
2022-08-22 18:00:39 +0300

    creating stack navigation for search screen

---
+++ navigation/searchStack.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import DATA from './config/Restaurant/DATA';
+// import recipes from './config/Restaurant/Recipes';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SearchScreen from '../Screens/Restaurant/SearchScreen';
+import FetchedDetail from '../Screens/Restaurant/FetchedDetail';
+
+const Stack = createNativeStackNavigator();
+
+const SearchStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='Search' component={SearchScreen} />
+      <Stack.Screen name='SearchDetails' component={FetchedDetail} />
+    </Stack.Navigator>
+  );
+};
+
+export default SearchStack;
