b24a73f8d390d0c3d003375f85e2364f951ada95
Hussein Omar <husseinomar6190@gmail.com>
2022-08-22 15:00:33 +0300

    creating a component to display the results fetched from the api

---
+++ componets/ResultsFound.js
@@ -0,0 +1,78 @@
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from 'react-native';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import SPACING from '../config/SPACING';
+import colors from '../config/Restaurant/colors';
+import DATA from '../config/Restaurant/DATA';
+import { useNavigation } from '@react-navigation/native';
+const { width } = Dimensions.get('window');
+
+const ITEM_WIDTH = width / 2 - SPACING * 3;
+
+const ResultsFound = ({ result }) => {
+  const navigation = useNavigation();
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          marginVertical: SPACING * 2,
+          padding: 20,
+        }}
+      >
+        {result?.meals?.map((item) => (
+          <TouchableOpacity
+            style={{ width: ITEM_WIDTH, marginBottom: SPACING * 2 }}
+            key={item.idMeal}
+            activeOpacity={0.7}
+            onPress={() =>
+              navigation.navigate('SearchDetails', { id: item.idMeal })
+            }
+          >
+            <Image
+              style={{
+                width: '100%',
+                height: ITEM_WIDTH + SPACING * 3,
+                borderRadius: SPACING * 2,
+              }}
+              source={{ uri: item.strMealThumb }}
+            />
+            <Text
+              style={{
+                fontSize: SPACING * 2,
+                fontWeight: '700',
+                marginTop: SPACING,
+              }}
+            >
+              {item.strMeal}
+            </Text>
+            <Text
+              style={{
+                fontSize: SPACING * 1.5,
+                color: colors.gray,
+                marginVertical: SPACING / 2,
+              }}
+            >
+              {item.strArea} - {item.strCategory}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ResultsFound;
