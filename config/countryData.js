dca8fe1a6b3a7e40a27efc5b7d05ad9880b8399a
Hussein Omar <husseinomar6190@gmail.com>
2022-08-24 20:52:45 +0300

    adding flag images

---
+++ config/countryData.js
@@ -0,0 +1,74 @@
+const countryList = [
+  {
+    id: 1,
+    name: 'American',
+    imgPath: require('../assets/flags/america.jpg'),
+  },
+  {
+    id: 2,
+    name: 'British',
+    imgPath: require('../assets/flags/british.jpg'),
+  },
+  {
+    id: 3,
+    name: 'Canadian',
+    imgPath: require('../assets/flags/canada.jpg'),
+  },
+  {
+    id: 4,
+    name: 'Chinese',
+    imgPath: require('../assets/flags/china.jpg'),
+  },
+  {
+    id: 5,
+    name: 'Croatian',
+    imgPath: require('../assets/flags/crotia.jpg'),
+  },
+  // {
+  //   id: 5,
+  //   name: 'Dutch',
+  // imgPath: require('../assets/flags/'),
+  // },
+  // {
+  //   id: 6,
+  //   name: 'Egyptian',
+  // imgPath: require('../assets/flags/'),
+  // },
+  // {
+  //   id: 7,
+  //   name: 'French',
+  //   imgPath: require(''),
+  // },
+  // {
+  //   id: 8,
+  //   name: 'Greek',
+  //   imgPath: require(''),
+  // },
+  // {
+  //   id: 9,
+  //   name: 'Indian',
+  //   imgPath: require(''),
+  // },
+  // {
+  //   id: 10,
+  //   name: 'Irish',
+  //   imgPath: require(''),
+  // },
+  // {
+  //   id: 11,
+  //   name: 'Italian',
+  //   imgPath: require(''),
+  // },
+  // {
+  //   id: 12,
+  //   name: 'Jamaican',
+  //   imgPath: require(''),
+  // },
+  // {
+  //   id: 13,
+  //   name: 'Japanese',
+  //   imgPath: require(''),
+  // },
+];
+
+export default countryList;
