51e8df54f236e61c22f0d8d85794637aae7844a2
Hussein Omar <husseinomar6190@gmail.com>
2022-09-11 22:49:07 +0300

    creating a simple login page

--- Screens/Restaurant/AuthPage.js
+++ Screens/Restaurant/AuthPage.js
@@ -1,17 +1,125 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  Alert,
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Pressable,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+// import { Button, SocialIcon } from 'react-native-elements';
+// import * as Facebook from 'expo-facebook';
 
-const AuthPage = () => {
-  const navigation = useNavigation();
+const appId = '1047121222092614';
+
+export default function AuthPage() {
+  const onLoginPress = () => {};
+
+  // const onFbLoginPress = async () => {
+  //   try {
+  //     await Facebook.initializeAsync({
+  //       appId,
+  //     });
+  //     const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+  //       permissions: ['public_profile', 'email'],
+  //     });
+  //     if (type === 'success') {
+  //       const response = await fetch(
+  //         `https://graph.facebook.com/me?access_token=${token}`
+  //       );
+  //       Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+  //     }
+  //   } catch ({ message }) {
+  //     Alert.alert(`Facebook Login Error: ${message}`);
+  //   }
+  // };
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Authentication page</Text>
-      </View>
-    </SafeAreaView>
+    <KeyboardAvoidingView style={styles.containerView} behavior='padding'>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+            <Text style={styles.logoText}>Receta</Text>
+            <TextInput
+              placeholder='Username'
+              placeholderColor='#c4c3cb'
+              style={styles.loginFormTextInput}
+            />
+            <TextInput
+              placeholder='Password'
+              placeholderColor='#c4c3cb'
+              style={styles.loginFormTextInput}
+              secureTextEntry={true}
+            />
+            <TouchableOpacity style={styles.loginButton}>
+              <Text style={{ color: '#fff', fontSize: 20, fontWeight: '700' }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+            {/* <Button
+              buttonStyle={styles.loginButton}
+              onPress={() => onLoginPress()}
+              title='Login'
+            />
+            <Button
+              containerStyle={styles.fbLoginButton}
+              type='clear'
+              onPress={() => onFbLoginPress()}
+              title='Login With Facebook'
+            /> */}
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
-};
+}
 
-export default AuthPage;
+const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  loginScreenContainer: {
+    flex: 1,
+  },
+  logoText: {
+    fontSize: 40,
+    fontWeight: '800',
+    marginTop: 150,
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  loginFormView: {
+    flex: 1,
+  },
+  loginFormTextInput: {
+    height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#eaeaea',
+    backgroundColor: '#fafafa',
+    paddingLeft: 10,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  loginButton: {
+    backgroundColor: 'tomato',
+    borderRadius: 5,
+    height: 45,
+    marginTop: 10,
+    width: 350,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fbLoginButton: {
+    height: 45,
+    marginTop: 10,
+    backgroundColor: 'transparent',
+  },
+});
--- navigation/SideNavigation.js
+++ navigation/SideNavigation.js
@@ -1,4 +1,5 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import AuthPage from '../Screens/Restaurant/AuthPage';
 import HomeScreen from '../Screens/Restaurant/HomeScreen';
 
 const Drawer = createDrawerNavigator();
@@ -7,6 +8,7 @@ export default function SideNavigation() {
   return (
     <Drawer.Navigator screenOptions={{ headerShown: false }}>
       <Drawer.Screen name='Home' component={HomeScreen} />
+      <Drawer.Screen name='Auth' component={AuthPage} />
     </Drawer.Navigator>
   );
 }
