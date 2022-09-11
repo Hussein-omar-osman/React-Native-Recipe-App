3e967ef6d8852ee9600c6550d49eeb66674b1fb4
Hussein Omar <husseinomar6190@gmail.com>
2022-09-11 23:20:44 +0300

    creating a signup screen

---
+++ componets/authComp/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Alert,
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Pressable,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
--- componets/authComp/Signup.js
+++ componets/authComp/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Alert,
   Image,
@@ -12,14 +12,9 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
-import Signup from '../../componets/authComp/Signup';
-// import { Button, SocialIcon } from 'react-native-elements';
-// import * as Facebook from 'expo-facebook';
+import styles from './styles';
 
-const appId = '1047121222092614';
-
-export default function AuthPage() {
-  const [page, setPage] = useState('register');
+export default function Signup() {
   const onLoginPress = () => {};
 
   // const onFbLoginPress = async () => {
@@ -41,9 +36,6 @@ export default function AuthPage() {
   //   }
   // };
 
-  if (page === 'register') {
-    return <Signup />;
-  }
   return (
     <KeyboardAvoidingView style={styles.containerView} behavior='padding'>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -56,75 +48,30 @@ export default function AuthPage() {
               style={styles.loginFormTextInput}
             />
             <TextInput
+              placeholder='Email'
+              placeholderColor='#c4c3cb'
+              style={styles.loginFormTextInput}
+            />
+            <TextInput
               placeholder='Password'
+              placeholderColor='#c4c3cb'
+              style={styles.loginFormTextInput}
+              secureTextEntry={true}
+            />
+            <TextInput
+              placeholder='Confirm Password'
               placeholderColor='#c4c3cb'
               style={styles.loginFormTextInput}
               secureTextEntry={true}
             />
             <TouchableOpacity style={styles.loginButton}>
               <Text style={{ color: '#fff', fontSize: 20, fontWeight: '700' }}>
-                Login
+                Register
               </Text>
             </TouchableOpacity>
-            {/* <Button
-              buttonStyle={styles.loginButton}
-              onPress={() => onLoginPress()}
-              title='Login'
-            />
-            <Button
-              containerStyle={styles.fbLoginButton}
-              type='clear'
-              onPress={() => onFbLoginPress()}
-              title='Login With Facebook'
-            /> */}
           </View>
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  containerView: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  loginScreenContainer: {
-    flex: 1,
-  },
-  logoText: {
-    fontSize: 40,
-    fontWeight: '800',
-    marginTop: 150,
-    marginBottom: 30,
-    textAlign: 'center',
-  },
-  loginFormView: {
-    flex: 1,
-  },
-  loginFormTextInput: {
-    height: 43,
-    fontSize: 14,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#eaeaea',
-    backgroundColor: '#fafafa',
-    paddingLeft: 10,
-    marginTop: 5,
-    marginBottom: 5,
-  },
-  loginButton: {
-    backgroundColor: 'tomato',
-    borderRadius: 5,
-    height: 45,
-    marginTop: 10,
-    width: 350,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  fbLoginButton: {
-    height: 45,
-    marginTop: 10,
-    backgroundColor: 'transparent',
-  },
-});
