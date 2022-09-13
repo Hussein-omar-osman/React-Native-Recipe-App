6c1cb7d9a6928ae829323a772a0ca527db8c24f0
Hussein Omar <husseinomar6190@gmail.com>
2022-09-13 22:42:18 +0300

    creating a starter for custom drawer

--- navigation/SideNavigation.js
+++ navigation/SideNavigation.js
@@ -1,12 +1,16 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AuthPage from '../Screens/Restaurant/AuthPage';
 import HomeScreen from '../Screens/Restaurant/HomeScreen';
+import CustomDrawer from './CustomDrawer';
 
 const Drawer = createDrawerNavigator();
 
 export default function SideNavigation() {
   return (
-    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+    <Drawer.Navigator
+      screenOptions={{ headerShown: false }}
+      drawerContent={(props) => <CustomDrawer {...props} />}
+    >
       <Drawer.Screen name='Home' component={HomeScreen} />
       <Drawer.Screen name='Auth' component={AuthPage} />
     </Drawer.Navigator>
