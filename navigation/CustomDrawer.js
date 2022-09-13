ee9870ef1a4ab326340c68e8f0924af306c58839
Hussein Omar <husseinomar6190@gmail.com>
2022-09-13 23:40:45 +0300

    created my custom drawer for sidenavogation

--- navigation/CustomDrawer.js
+++ navigation/CustomDrawer.js
@@ -1,10 +1,89 @@
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
-const CustomDrawer = () => {
+const CustomDrawer = (props) => {
   return (
-    <View>
-      <Text>Custom drawer</Text>
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{ backgroundColor: 'tomato' }}
+      >
+        <View style={{ padding: 20 }}>
+          <Image
+            source={require('../assets/unknow.png')}
+            style={{
+              marginBottom: 10,
+              width: 80,
+              height: 80,
+              borderRadius: 40,
+            }}
+          />
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 18,
+            }}
+          >
+            @unKnown
+          </Text>
+        </View>
+        <View style={{ backgroundColor: '#fff', flex: 1, paddingTop: 20 }}>
+          <DrawerItemList {...props} />
+        </View>
+      </DrawerContentScrollView>
+      <View style={{ padding: 10, borderTopWidth: 1, borderColor: '#ccc' }}>
+        <TouchableOpacity
+          style={{ paddingVertical: 15 }}
+          onPress={() => props.navigation.closeDrawer()}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <Entypo name='share' size={22} color='black' />
+            <Text
+              style={{
+                color: '#333',
+                fontSize: 15,
+                marginLeft: 5,
+                fontWeight: 'bold',
+              }}
+            >
+              Tell a friend
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ paddingVertical: 15 }}
+          onPress={() => props.navigation.closeDrawer()}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <Entypo name='log-out' size={22} color='black' />
+            <Text
+              style={{
+                color: '#333',
+                fontSize: 15,
+                marginLeft: 5,
+                fontWeight: 'bold',
+              }}
+            >
+              Sign out
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- navigation/SideNavigation.js
+++ navigation/SideNavigation.js
@@ -2,17 +2,40 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import AuthPage from '../Screens/Restaurant/AuthPage';
 import HomeScreen from '../Screens/Restaurant/HomeScreen';
 import CustomDrawer from './CustomDrawer';
-
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 const Drawer = createDrawerNavigator();
 
 export default function SideNavigation() {
   return (
     <Drawer.Navigator
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: 'tomato',
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#333',
+        drawerLabelStyle: { marginLeft: -25, fontSize: 15 },
+      }}
       drawerContent={(props) => <CustomDrawer {...props} />}
     >
-      <Drawer.Screen name='Home' component={HomeScreen} />
-      <Drawer.Screen name='Auth' component={AuthPage} />
+      <Drawer.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name='md-home-sharp' size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name='Auth'
+        component={AuthPage}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Entypo name='login' size={size} color={color} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
