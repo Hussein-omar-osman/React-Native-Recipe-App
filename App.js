e044260d588798cc45fc387f3b0cab72d57c1e03
Hussein Omar <husseinomar6190@gmail.com>
2022-09-16 01:29:17 +0300

    Changing the style and statusbar and removing unused imports

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import RecipeDetailScreen from './Screens/Restaurant/RecipeDetailScreen';
-import DATA from './config/Restaurant/DATA';
-import recipes from './config/Restaurant/Recipes';
-import HomeScreen from './Screens/Restaurant/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from './Screens/Restaurant/WelcomeScreen';
-import HomeStack from './navigation/homeStack';
 import BottomTabsNavigation from './navigation/BottomTabsNavigation';
-import CategoryScreen from './Screens/Restaurant/CategoryScreen';
 import BottomSheetProvider from './context/BottomSheetContext';
 import { StatusBar } from 'expo-status-bar';
 
@@ -21,7 +14,7 @@ const App = () => {
       <BottomSheetProvider>
         <BottomTabsNavigation />
       </BottomSheetProvider>
-      <StatusBar />
+      <StatusBar style='auto' />
     </NavigationContainer>
   );
 };
