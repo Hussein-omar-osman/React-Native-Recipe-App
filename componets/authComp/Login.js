192ac7febdb7e2299fbe2137ce9545b1ad9dfb8f
Hussein Omar <husseinomar6190@gmail.com>
2022-09-16 18:37:04 +0300

    showing and hidding in a text input with type password

--- componets/authComp/Login.js
+++ componets/authComp/Login.js
@@ -7,12 +7,26 @@ import {
   TouchableWithoutFeedback,
   View,
   TouchableOpacity,
+  Vibration,
+  Alert,
 } from 'react-native';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Login({ setPage }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [visible, setVisibility] = useState(true);
+  const icon = !visible ? 'eye-slash' : 'eye';
+
+  const onSubmit = () => {
+    if (email == '' || password == '') {
+      Alert.alert('Missing Data', 'Provide all the required Data for login');
+      return;
+    }
+    Alert.alert('Credentials', `Email: ${email}/n Password: ${password}`);
+  };
+
   return (
     <KeyboardAvoidingView style={styles.containerView} behavior='padding'>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -20,17 +34,38 @@ export default function Login({ setPage }) {
           <View style={styles.loginFormView}>
             <Text style={styles.logoText}>Receta</Text>
             <TextInput
-              placeholder='Username'
+              placeholder='Email'
               placeholderColor='#c4c3cb'
               style={styles.loginFormTextInput}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
             />
-            <TextInput
-              placeholder='Password'
-              placeholderColor='#c4c3cb'
-              style={styles.loginFormTextInput}
-              secureTextEntry={true}
-            />
-            <TouchableOpacity style={styles.loginButton}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <TextInput
+                placeholder='Password'
+                placeholderColor='#c4c3cb'
+                style={[styles.loginFormTextInput, { flex: 2 }]}
+                secureTextEntry={visible}
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+              />
+              <Icon
+                name={icon}
+                color={'#9e9e9e'}
+                size={24}
+                onPress={() => {
+                  setVisibility((prev) => !prev);
+                  Vibration.vibrate(100);
+                }}
+              />
+            </View>
+            <TouchableOpacity style={styles.loginButton} onPress={onSubmit}>
               <Text style={{ color: '#fff', fontSize: 20, fontWeight: '700' }}>
                 Login
               </Text>
--- componets/authComp/Signup.js
+++ componets/authComp/Signup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Keyboard,
   KeyboardAvoidingView,
@@ -7,14 +7,30 @@ import {
   TouchableWithoutFeedback,
   View,
   TouchableOpacity,
+  Vibration,
 } from 'react-native';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Signup({ setPage }) {
-  const [userName, setUserName] = useState('');
+  // const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [ConPassword, setConPassword] = useState('');
+  const [conPassword, setConPassword] = useState('');
+  const [error, setError] = useState('');
+  const [visible1, setVisibility1] = useState(true);
+  const [visible2, setVisibility2] = useState(true);
+  const icon1 = !visible1 ? 'eye-slash' : 'eye';
+  const icon2 = !visible2 ? 'eye-slash' : 'eye';
+
+  const handleSignUp = () => {
+    if (password !== conPassword) {
+      setError("Password & Re-password don't match");
+      return;
+    }
+    console.log('Success the password match');
+  };
+
   return (
     <KeyboardAvoidingView style={styles.containerView} behavior='padding'>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -22,27 +38,67 @@ export default function Signup({ setPage }) {
           <View style={styles.loginFormView}>
             <Text style={styles.logoText}>Receta</Text>
             <TextInput
-              placeholder='Username'
-              placeholderColor='#c4c3cb'
-              style={styles.loginFormTextInput}
-            />
-            <TextInput
               placeholder='Email'
               placeholderColor='#c4c3cb'
               style={styles.loginFormTextInput}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
             />
-            <TextInput
-              placeholder='Password'
-              placeholderColor='#c4c3cb'
-              style={styles.loginFormTextInput}
-              secureTextEntry={true}
-            />
-            <TextInput
-              placeholder='Confirm Password'
-              placeholderColor='#c4c3cb'
-              style={styles.loginFormTextInput}
-              secureTextEntry={true}
-            />
+
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <TextInput
+                placeholder='Password'
+                placeholderColor='#c4c3cb'
+                style={[styles.loginFormTextInput, { flex: 2 }]}
+                secureTextEntry={visible1}
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+              />
+              <Icon
+                name={icon1}
+                color={'#9e9e9e'}
+                size={24}
+                onPress={() => {
+                  setVisibility1((prev) => !prev);
+                  Vibration.vibrate(100);
+                }}
+                style={{ backgroundColor: '#fff', padding: 7 }}
+              />
+            </View>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <TextInput
+                placeholder='Confirm Password'
+                placeholderColor='#c4c3cb'
+                style={[styles.loginFormTextInput, { flex: 2 }]}
+                secureTextEntry={visible2}
+                value={conPassword}
+                onChangeText={(text) => setConPassword(text)}
+              />
+              <Icon
+                name={icon2}
+                color={'#9e9e9e'}
+                size={24}
+                onPress={() => {
+                  setVisibility2((prev) => !prev);
+                  Vibration.vibrate(100);
+                }}
+                style={{ backgroundColor: '#fff', padding: 7 }}
+              />
+            </View>
+
             <TouchableOpacity style={styles.loginButton}>
               <Text style={{ color: '#fff', fontSize: 20, fontWeight: '700' }}>
                 Register
